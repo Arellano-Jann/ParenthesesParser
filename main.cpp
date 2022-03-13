0d4097c4bc8d4ccf5f80dbd03d74afc095d3b176
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-03-13 10:25:43 -0700

    Fixed parentheses

--- main.cpp
+++ main.cpp
@@ -47,13 +47,13 @@ int main(){
             // bool currentValidity = (parentheses == 0 && curly == 0 && square == 0);
             bool currentlyValid = (parentheses + curly + square == 0);
 
-			if ( i == closedParentheses && ( ((closedParentheses + openParentheses) % 2) != ((openParentheses+1) % 2)) ) return false;
+			if ( parentheses == 0 && i == closedParentheses && ( ((closedParentheses + openParentheses) % 2) != ((openParentheses+1) % 2)) ) return false;
 			// else{ return false; }
 
-			if ( i == closedCurly && ( ((closedCurly + openCurly) % 2) != ((openCurly+1) % 2)) ) return false;
+			if ( curly == 0 && i == closedCurly && ( ((closedCurly + openCurly) % 2) != ((openCurly+1) % 2)) ) return false;
 			// else{ return false; }
 
-			if ( i == closedSquare && ( ((closedCurly + openSquare) % 2) != ((openSquare+1) % 2)) ) return false;
+			if ( square == 0 && i == closedSquare && ( ((closedCurly + openSquare) % 2) != ((openSquare+1) % 2)) ) return false;
 			// else{ return false; }
             // for (int j = i+1; j < s.length(); j++){
                 // if (s[i] == map[3]) valid = currentlyValid;
