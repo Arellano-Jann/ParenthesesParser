49a7e5d352cd281a4808e88df7a8751a68a22b94
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-03-13 09:59:01 -0700

    Fixed all 0 Problem

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ bool isValid(std::string s);
 int main(){
 	std::cout << isValid("()") << std::endl; // true
 	std::cout << isValid("()[]{}") << std::endl; // true
-	std::cout << isValid("()") << std::endl; // true
+	std::cout << isValid("(())") << std::endl; // true
 	std::cout << isValid("({})") << std::endl; // true
 	std::cout << isValid("{}") << std::endl; // true
 	std::cout << std::endl;
@@ -22,12 +22,12 @@ int main(){
         int parentheses = 0;
         int curly = 0;
         int square = 0;
-		int openParentheses = 0;
-        int openCurly = 0;
-        int openSquare = 0;
-		int closedParentheses = 0;
-        int closedCurly = 0;
-        int closedSquare = 0;
+		int openParentheses = -1;
+        int openCurly = -1;
+        int openSquare = -1;
+		int closedParentheses = -1;
+        int closedCurly = -1;
+        int closedSquare = -1;
         
         for (int i = 0; i < s.length(); i++){
             if ( s[i] == map[0] ){ openParentheses = i; parentheses++; }
@@ -47,11 +47,14 @@ int main(){
             // bool currentValidity = (parentheses == 0 && curly == 0 && square == 0);
             bool currentlyValid = (parentheses + curly + square == 0);
 
-			if ( i == closedParentheses && ( (closedParentheses + openParentheses) % 2 == (openParentheses + 1) % 2) ) valid = currentlyValid;
+			if ( i == closedParentheses && ( ((closedParentheses + openParentheses) % 2) != ((openParentheses+1) % 2)) ) return false;
+			// else{ return false; }
 
-			if ( i == closedCurly && ( (closedCurly + openCurly) % 2 == (openCurly + 1) % 2) ) valid = currentlyValid;
+			if ( i == closedCurly && ( ((closedCurly + openCurly) % 2) != ((openCurly+1) % 2)) ) return false;
+			// else{ return false; }
 
-			if ( i == closedSquare && ( (closedCurly + openSquare) % 2 == (openSquare + 1) % 2) ) valid = currentlyValid;
+			if ( i == closedSquare && ( ((closedCurly + openSquare) % 2) != ((openSquare+1) % 2)) ) return false;
+			// else{ return false; }
             // for (int j = i+1; j < s.length(); j++){
                 // if (s[i] == map[3]) valid = currentlyValid;
                 // if (s[i] == map[4]) valid = currentlyValid;
